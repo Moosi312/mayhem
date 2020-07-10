4863b28f4912bba294ce5990dc55645844772417
Moosi312 <42666394+Moosi312@users.noreply.github.com>
2020-07-10 16:28:57 +0200

    Really fix exit clauses

--- spam/jwort/scraper.py
+++ spam/jwort/scraper.py
@@ -100,7 +100,6 @@ def createSession(session):
         page = session.get("https://www.surveymonkey.com/r/7JZRVLJ?embedded=1")
     except requests.RequestException:
         print("[!] Can't connect to host. Do you have an existing internet connection?")
-        exit()
 
     survey_data_offset = page.text.find("survey_data\" value=\"") + 20
 

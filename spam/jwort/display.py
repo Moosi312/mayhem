664f079db445fb0abe60983323aa019036342cd3
Moosi312 <42666394+Moosi312@users.noreply.github.com>
2020-07-20 19:56:38 +0200

    Update display.py

--- spam/jwort/display.py
+++ spam/jwort/display.py
@@ -3,7 +3,9 @@ import json
 with open("count.json", 'r') as f:
     count = json.load(f)
 
+print()
+print("Posted words:")
 for word in count:
-    print("\"" + word + "\" has been posted " + str(count[word]) + " times.")
+    print("\t\"" + word + "\" has been posted " + str(count[word]) + " times.")
           
 print()
